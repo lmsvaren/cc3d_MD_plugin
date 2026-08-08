493fe1d86198c7d93cbd923bfa995b24757f04be
lmsvaren <149616682+lmsvaren@users.noreply.github.com>
2026-08-07 22:29:52 -0500

    Update AdhesiveSatPlugin

--- AdhesiveSat/AdhesiveSatPlugin.cpp
+++ AdhesiveSat/AdhesiveSatPlugin.cpp
@@ -1,5 +1,4 @@
 #include <CompuCell3D/CC3D.h>        
-
 #include "AdhesiveSatPlugin.h"
 #include <algorithm>
 #include <cmath>
@@ -15,26 +14,19 @@ AdhesiveSatPlugin::AdhesiveSatPlugin():
     E0(50.0),
     Aref(250.0),
     pUtils(nullptr),
-    lockPtr(0),
-// xmlData(0) ,
-// boundaryStrategy(0)
+    lockPtr(nullptr),
+    xmlData(nullptr),
+    automaton(nullptr),
+    boundaryStrategy(nullptr)
 {}
 
 AdhesiveSatPlugin::~AdhesiveSatPlugin() {
-
     if (pUtils && lockPtr) {
         pUtils->destroyLock(lockPtr);
         delete lockPtr;
         lockPtr = nullptr;
     }
-
-
-    delete lockPtr;
-
-    lockPtr=0;
-
 }
-
 
 int AdhesiveSatPlugin::getOccupiedSiteCount(const CellG* cell) {
     if (!cell) {
@@ -53,9 +45,9 @@ int AdhesiveSatPlugin::getOccupiedSiteCount(const CellG* cell) {
 }
 
 void AdhesiveSatPlugin::init(Simulator *simulator, CC3DXMLElement *_xmlData) {
-    sim=simulator;
-    potts=simulator->getPotts();
-    cellFieldG = static cast<WatchableField3D<CellG*> *>(
+    sim = simulator;
+    potts = simulator->getPotts();
+    cellFieldG = static_cast<WatchableField3D<CellG*> *>(
         potts->getCellFieldG()
     );
     
@@ -66,86 +58,61 @@ void AdhesiveSatPlugin::init(Simulator *simulator, CC3DXMLElement *_xmlData) {
         fieldDim.x == 200 &&
         fieldDim.y == 200 &&
         fieldDim.z == 1
-
-    )
-
+    );
 
     potts->getCellFactoryGroupPtr()->registerClass(
         &adhesiveSatDataAccessor
     );
 
-    /*
-    Create numpy backed scalar field
-    unsigned char used as binary type
-    0 = False
-    1 = True
-    */
     sim->createGenericScalarField<unsigned char>(
         adhesionFieldName
     );
 
-    adhesionField=
-        sim->getGenericScalarField<unsigned char>(
-            adhesionFieldName
-        );
+    adhesionField = sim->getGenericScalarField<unsigned char>(
+        adhesionFieldName
+    );
 
-    xmlData=_xmlData;
-    
+    xmlData = _xmlData;
+    pUtils = sim->getParallelUtils();
 
-
-    pUtils=sim->getParallelUtils();
-
-    lockPtr=new ParallelUtilsOpenMP::OpenMPLock_t;
-
+    lockPtr = new ParallelUtilsOpenMP::OpenMPLock_t;
     pUtils->initLock(lockPtr); 
 
     update(_xmlData, true);
 
-    potts->registerEnergyFunctionWithName(this,"AdhesiveSat");
+    potts->registerEnergyFunctionWithName(this, "AdhesiveSat");
 
     // field3dChange called after successful pixel changes
     potts->registerCellGChangeWatcher(this);    
     simulator->registerSteerableObject(this);
-
 }
 
-            
-
-void AdhesiveSatPlugin::field3DChange(const Point3D &pt, CellG *newCell, CellG *oldCell) 
-
-{
-
-    //This function will be called after each succesful pixel copy - field3DChange does usuall ohusekeeping tasks to make sure state of cells, and state of the lattice is uptdate
-    if (!isAdhesiveSite(pt)) { //no point evaluating if not an adhesive site
-		return;
-	}
+void AdhesiveSatPlugin::field3DChange(const Point3D &pt, CellG *newCell, CellG *oldCell) {
+    if (!isAdhesiveSite(pt)) { 
+        return;
+    }
     pUtils->setLock(lockPtr); 
-	if (oldCell) {
-		AdhesiveSatData* oldData =adhesiveSatDataAccessor.get(
-                	oldCell->extraAttribPtr);
-		--oldData->occupiedAdhesiveSites; //old cell loses a site
-    	}
-	if (newCell) {
-        		AdhesiveSatData* newData =adhesiveSatDataAccessor.get(
-               	newCell->extraAttribPtr);
-		++newData->occupiedAdhesiveSites; //new cell gains a site
-    	}
+    if (oldCell) {
+        AdhesiveSatData* oldData = adhesiveSatDataAccessor.get(oldCell->extraAttribPtr);
+        --oldData->occupiedAdhesiveSites; 
+    }
+    if (newCell) {
+        AdhesiveSatData* newData = adhesiveSatDataAccessor.get(newCell->extraAttribPtr);
+        ++newData->occupiedAdhesiveSites; 
+    }
     pUtils->unsetLock(lockPtr); 
-    
-
 }
 
-
-void AdhesiveSatPlugin::initializeYField(){
+void AdhesiveSatPlugin::initializeYField() {
     const int centerX = 100;
     const int halfWidth = 3;
     
-    for (int x = 0; x <fieldDim.x; ++x){
-        for (int y = 0; y <fieldDim.y; ++y){
-            Point3D pt(x,y,0)
+    for (int x = 0; x < fieldDim.x; ++x) {
+        for (int y = 0; y < fieldDim.y; ++y) {
+            Point3D pt(x, y, 0);
             const bool stem = 
                 y >= 20 &&
-                y >= 100 &&
+                y <= 100 &&
                 std::abs(x - centerX) <= halfWidth;
 
             const bool leftArm =
@@ -158,103 +125,83 @@ void AdhesiveSatPlugin::initializeYField(){
                 y <= 160 &&
                 std::abs(x - y) <= halfWidth;
 
-            const bool adhesionMoleculesPresent =
-                stem || leftArm || rightArm;
+            const bool adhesionMoleculesPresent = stem || leftArm || rightArm;
 
             adhesionField->set(
                 pt,
-                static_cast<unsigned char>(
-                    adhesionMoleculesPresent
-                )
+                static_cast<unsigned char>(adhesionMoleculesPresent)
             );
         }
     }
 }
 
-
 bool AdhesiveSatPlugin::isAdhesiveSite(const Point3D& pt) const {
     if (!adhesionField->isValid(pt)) {
-        return False;
+        return false;
     }
     return adhesionField->get(pt) != 0;
 }
 
-
-void AdhesiveSatPlugin::initializeOccuppiedSiteCounts(){
-    CellInventory* CellInventoryPtr = 
-        &potts->getCellInventory();
-
+void AdhesiveSatPlugin::initializeOccupiedSiteCounts() {
+    CellInventory* cellInventoryPtr = &potts->getCellInventory();
     CellInventory::cellInventoryIterator cInvItr;
 
     for (
-        cInvItr = CellInventoryPtr->cellInventoryBegin();
-        cInvItr != CellInventoryPtr->cellInventoryEnd();
+        cInvItr = cellInventoryPtr->cellInventoryBegin();
+        cInvItr != cellInventoryPtr->cellInventoryEnd();
         ++cInvItr
     ) {
-        CellG* cell = 
-            CellInventoryPtr->getCell(cInvItr);
+        CellG* cell = cellInventoryPtr->getCell(cInvItr);
 
-        AdhesiveSatData* data = 
-            adhesiveSatDataAccessor.get(
-                cell->extraAttribPtr
-            );
+        AdhesiveSatData* data = adhesiveSatDataAccessor.get(cell->extraAttribPtr);
         data->occupiedAdhesiveSites = 0;
     }
 
-    for (int x = 0; x <fieldDim.x; ++x){
-        for (int y = 0; y <fieldDim.y; ++y){
-            Point3D pt(x,y,0)
+    for (int x = 0; x < fieldDim.x; ++x) {
+        for (int y = 0; y < fieldDim.y; ++y) {
+            Point3D pt(x, y, 0);
             if (!isAdhesiveSite(pt)) {
                 continue;
             }
             CellG* cell = cellFieldG->get(pt);
 
-            if (!cell){
-                continue
+            if (!cell) {
+                continue;
             }
 
-            AdhesiveSiteData* data = 
-                adhesiveSatDataAccessor.get(
-                    cell->extraAttribPtr
-                );
+            AdhesiveSatData* data = adhesiveSatDataAccessor.get(cell->extraAttribPtr);
             ++data->occupiedAdhesiveSites;
         }
     }
 }
 
-
-
 double AdhesiveSatPlugin::adhesionEnergy(double occupiedArea) const {
-    if (occupiedArea <= 0.0){
+    if (occupiedArea <= 0.0) {
         return 0.0;
     }
-	// return -E0 * occupiedArea /(Aref + occupiedArea);
-    return -E0 * occupiedArea /(Aref + occupiedArea);
+    return -E0 * occupiedArea / (Aref + occupiedArea);
 }
 
-double AdhesiveSatPlugin::changeEnergy(const Point3D &pt,const CellG *newCell,const CellG *oldCell) {
-	if (!isAdhesiveSite(pt)) { //no point evaluating if not an adhesive site
-        		return 0.0;
-    	}
-    double deltaH = 0.0; //initialize energy
+double AdhesiveSatPlugin::changeEnergy(const Point3D &pt, const CellG *newCell, const CellG *oldCell) {
+    if (!isAdhesiveSite(pt)) { 
+        return 0.0;
+    }
+    double deltaH = 0.0; 
     if (oldCell) {
-            const int oldAi =getOccupiedSiteCount(oldCell);
-            const int proposedAi = oldAi - 1; //old cell would lose a site
-            deltaH += adhesionEnergy(proposedAi) - adhesionEnergy(oldAi);
-        }
+        const int oldAi = getOccupiedSiteCount(oldCell);
+        const int proposedAi = oldAi - 1; 
+        deltaH += adhesionEnergy(proposedAi) - adhesionEnergy(oldAi);
+    }
     if (newCell) {
-        const int oldAi =getOccupiedSiteCount(newCell); 
-        const int proposedAi = oldAi + 1; //new cell would gain a site
-            deltaH +=adhesionEnergy(proposedAi) -adhesionEnergy(oldAi);
-        }
-        return deltaH;
+        const int oldAi = getOccupiedSiteCount(newCell); 
+        const int proposedAi = oldAi + 1; 
+        deltaH += adhesionEnergy(proposedAi) - adhesionEnergy(oldAi);
+    }
+    return deltaH;
 }
 
-
-
-void AdhesiveSatPlugin::update(CC3DXMLElement *_xmlData, bool _fullInitFlag){
-
-    if (!_xmlData){
+void AdhesiveSatPlugin::update(CC3DXMLElement *_xmlData, bool _fullInitFlag) {
+    if (!_xmlData) {
         return;
     }
     CC3DXMLElement* e0Element = _xmlData->getFirstElement("E0");
@@ -265,65 +212,26 @@ void AdhesiveSatPlugin::update(CC3DXMLElement *_xmlData, bool _fullInitFlag){
 
     CC3DXMLElement* aRefElement = _xmlData->getFirstElement("Aref");
 
-    if (aRef) {
+    if (aRefElement) {
         Aref = aRefElement->getDouble();
     }
 
     ASSERT_OR_THROW(
-        "AdhesiveStat Aref must be greater than zero"
+        "AdhesiveSat Aref must be greater than zero",
         Aref > 0
     );
-
-
-//     //PARSE XML IN THIS FUNCTION
-
-//     //For more information on XML parser function please see CC3D code or lookup XML utils API
-
-//     automaton = potts->getAutomaton();
-
-//     ASSERT_OR_THROW("CELL TYPE PLUGIN WAS NOT PROPERLY INITIALIZED YET. MAKE SURE THIS IS THE FIRST PLUGIN THAT YOU SET", automaton)
-
-//    set<unsigned char> cellTypesSet;
-
-
-//     if (exampleXMLElem){
-
-//         double param=exampleXMLElem->getDouble();
-
-//         cerr<<"param="<<param<<endl;
-
-//         if(exampleXMLElem->findAttribute("Type")){
-
-//             std::string attrib=exampleXMLElem->getAttribute("Type");
-
-//             // double attrib=exampleXMLElem->getAttributeAsDouble("Type"); //in case attribute is of type double
-
-//             cerr<<"attrib="<<attrib<<endl;
-
-//         }
-
-//     }
-
-//     //boundaryStrategy has information aobut pixel neighbors 
-
-//     boundaryStrategy=BoundaryStrategy::getInstance();
-
 }
-
-
 
 void AdhesiveSatPlugin::extraInit(Simulator* simulator) {
-	initializeYField();
-	//this function creates a 200x200 lattice forming a Y with thickness 7
-	initializeOccupiedSiteCounts();
-	//this function initializes the number of lattice sites already occupied by cells
+    initializeYField();
+    initializeOccupiedSiteCounts();
 }
 
-std::string AdhesiveSatPlugin::toString(){
+std::string AdhesiveSatPlugin::toString() {
     return "AdhesiveSat";
 }
 
-std::string AdhesiveSatPlugin::steerableName(){
+std::string AdhesiveSatPlugin::steerableName() {
     return toString();
 }
 
--- AdhesiveSat/AdhesiveSatPlugin.h
+++ AdhesiveSat/AdhesiveSatPlugin.h
@@ -22,14 +22,12 @@ namespace CompuCell3D {
 
     class ADHESIVESAT_EXPORT AdhesiveSatData {
     public:
-        int: occupiedAdhesiveSites;
+        int occupiedAdhesiveSites;
 
-        AdhesiveSatData() :
-            occupiedAdhesiveSites(0) {
-            }
-    }
+        AdhesiveSatData() : occupiedAdhesiveSites(0) {}
+    };
 
-    class ADHESIVESAT_EXPORT  AdhesiveSatPlugin : public Plugin ,public EnergyFunction ,public CellGChangeWatcher {
+    class ADHESIVESAT_EXPORT AdhesiveSatPlugin : public Plugin, public EnergyFunction, public CellGChangeWatcher {
 
     private:            
         CC3DXMLElement *xmlData;        
@@ -42,17 +40,17 @@ namespace CompuCell3D {
         WatchableField3D<CellG *> *cellFieldG;
 
         void initializeYField();
-        void initializeOccuppiedSiteCounts();
+        void initializeOccupiedSiteCounts();
         bool isAdhesiveSite(const Point3D& pt) const;
         int getOccupiedSiteCount(const CellG* cell);
         double adhesionEnergy(double occupiedArea) const;
 
+        Dim3D fieldDim;
         Field3D<unsigned char>* adhesionField;
         std::string adhesionFieldName;
-		ExtraMembersGroupAccessor<AdhesiveSatData>
-            adhesiveSatDataAccessor;
+        ExtraMembersGroupAccessor<AdhesiveSatData> adhesiveSatDataAccessor;
 
-		double E0;
+        double E0;
         double Aref;
 
     public:
@@ -61,45 +59,38 @@ namespace CompuCell3D {
         
         virtual void init(
             Simulator *simulator, 
-            CC3DXMLElement *_xmlData=0
+            CC3DXMLElement *_xmlData = 0
         );
 
         virtual void extraInit(Simulator *simulator);
 
-
-        //Energy function interface
+        // Energy function interface
         virtual double changeEnergy(
             const Point3D &pt, 
             const CellG *newCell, 
             const CellG *oldCell
         );        
+        
         // CellChangeWatcher interface
         virtual void field3DChange(
             const Point3D &pt, 
             CellG *newCell, 
             CellG *oldCell
         );
-                
 
-        
-
-        //Steerrable interface
-
+        // Steerable interface
         virtual void update(
             CC3DXMLElement *_xmlData, 
-            bool _fullInitFlag=false
+            bool _fullInitFlag = false
         );
         virtual std::string steerableName();
-
         virtual std::string toString();
 
-        ExtraMembersGroupAccessor<AdhesiveSatData>*
-            getAdhesiveSatDataAccessorPtr() {
+        ExtraMembersGroupAccessor<AdhesiveSatData>* getAdhesiveSatDataAccessorPtr() {
             return &adhesiveSatDataAccessor;
         }
-
     };
 
-};
+}
 
 #endif
